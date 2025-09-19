0162c5a371caf587f40863ea5de765bf13d7bbb4
Sefora-Davanso <seforadavanso@gmail.com>
2025-09-18 22:05:05 -0300

    De nada Levorato!

---
+++ Bootstrap/src/data/alunos.js
@@ -0,0 +1,8 @@
+export const alunos = [
+    { nome: 'João', sexo: 'masculino' },
+    { nome: 'Maria', sexo: 'feminino' },
+    { nome: 'Pedro', sexo: 'masculino' },
+    { nome: 'Ana', sexo: 'feminino' },
+    { nome: 'Lucas', sexo: 'masculino' },
+    { nome: 'Carla', sexo: 'feminino' }
+];
